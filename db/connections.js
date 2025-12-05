682e6fc5b6fbee41b2e72466345e84a602b03296
Steven Winter <stevenwinter.me@gmail.com>
2025-12-04 19:50:01 -0500

    Updated connections.js. Removed bytes dependency.

--- db/connections.js
+++ db/connections.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose')
- 
-const mongoURI = 
-  process.env.NODE_ENV === 'production' 
-  ? process.env.DB_URL 
-  : 'mongodb://localhost/coffee-app'
-  
+const mongoose = require('mongoose');
+
+const mongoURI =
+  process.env.NODE_ENV === 'production'
+    ? process.env.DB_URL
+    : 'mongodb://localhost/coffee-app';
+
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  .connect(mongoURI) // Removed unsupported options
   .then((instance) =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
